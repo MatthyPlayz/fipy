062a1a93d3f9a04c392b7d1052af0aa3a9d955d6
matthyno <34276633+MatthyPlayz@users.noreply.github.com>
2020-01-04 10:40:56 -0500

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-from InitGen import *
-from Item import *
-InitDir("com.matthyfamily")
-ItemBasicGen("com.matthyfamily")
-RefGen("com.matthyfamily", "brinify", "Brinify", "1.0", "1.12.2")
-InitGen(["com.matthyfamily.Brine"], "com.matthyfamily", "Brinify.java", ["Brine"])
-ModItems("brinify", "com.matthyfamily", "Brine.java", "brine")
-os.system("gradlew build")
